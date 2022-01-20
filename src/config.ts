4f85148872a1b4a1c3d3653ca23de5dbb5c43fdc
Masaki Hara <ackie.h.gmai@gmail.com>
2022-01-20 14:21:26 +0900

    Allow configuring output path

---
+++ src/config.ts
@@ -0,0 +1,62 @@
+export type Config = {
+  rules?: Rule[] | undefined;
+  output?: string | undefined;
+};
+
+export type Rule = {
+  include: string[];
+  exclude?: string[] | undefined;
+  parser: string;
+  options?: any;
+};
+
+class ConfigError extends Error {
+  constructor(path: string, message?: string) {
+    const reducedPath = path.replace(/^\$\./, "");
+    super(reducedPath === "$" ? `Config error: ${message}` : `Config error: ${reducedPath}: ${message}`)
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ConfigError)
+    }
+
+    this.name = ConfigError.name
+  }
+}
+
+export function validateConfig(obj: unknown, path = "$"): asserts obj is Config {
+  if (typeof obj !== "object" || obj === null) throw new ConfigError(path, "not an object");
+  validateKeys(obj, path, ["rules", "output"] as const);
+  if (typeof obj.rules !== "undefined") {
+    validateArray(obj.rules, `${path}.rules`, validateRule);
+  }
+  if (typeof obj.output !== "undefined") {
+    validateString(obj.output, `${path}.output`);
+  }
+}
+
+export function validateRule(obj: unknown, path = "$"): asserts obj is Rule {
+  if (typeof obj !== "object" || obj === null) throw new ConfigError(path, "not an object");
+  validateKeys(obj, path, ["include", "exclude", "parser", "options"] as const);
+  validateArray(obj.include, `${path}.include`, validateString);
+  if (typeof obj.exclude !== "undefined") {
+    validateArray(obj.exclude, `${path}.exclude`, validateString);
+  }
+  validateString(obj.parser, `${path}.parser`);
+}
+
+function validateString(obj: unknown, path: string): asserts obj is string {
+  if (typeof obj !== "string") throw new ConfigError(path, "not a string");
+}
+
+function validateKeys<Keys extends readonly string[]>(obj: object, path: string, keys: Keys): asserts obj is Partial<Record<Keys[number], unknown>> {
+  for (const key of Object.keys(obj)) {
+    if (!keys.some((k) => k === key)) throw new ConfigError(path, `Unrecognized key: ${key}`);
+  }
+}
+
+function validateArray<T>(obj: unknown, path: string, subValidator: (elem: unknown, path: string) => asserts elem is T): asserts obj is T[] {
+  if (!Array.isArray(obj)) throw new ConfigError(path, "not an array");
+  obj.forEach((elem, index) => {
+    subValidator(elem, `${path}[${index}]`);
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import glob from "glob";
 import fs from "node:fs";
+import path from "node:path";
 import { promisify } from "node:util";
 import { program } from "commander";
+import { cosmiconfig } from "cosmiconfig";
 import { parse as parseHaml } from "./haml";
 import { parse as parseErb } from "./erb";
+import { validateConfig } from "./config";
 
 program.version("0.1.0");
 program.name("unused-classes");
@@ -15,6 +18,11 @@ const opts = program.opts();
 
 (async () => {
   const { directory = "." } = opts;
+  const explorer = cosmiconfig("classnames");
+  const config = await explorer.search(directory);
+  if (!config.config) throw new Error("Config not found");
+  validateConfig(config.config);
+  const { output = "classes.json" } = config.config;
   const files = await promisify(glob)(`${directory}/app/**/*`, { nodir: true });
   const classNames = new Set<string>();
   for (const filename of files) {
@@ -33,5 +41,5 @@ const opts = program.opts();
   const result = {
     class_names: Array.from(classNames.values()).sort(),
   };
-  await fs.promises.writeFile(`${directory}/tmp/classes.json`, JSON.stringify(result));
+  await fs.promises.writeFile(path.resolve(directory, output), JSON.stringify(result));
 })();
